a44da97b743cf5f131bb6a7709ba72e30701be8b
Adam Jackson <ajax@nwnk.net>
2004-08-03 02:44:21 +0000

    Bug #962: Remove LoaderSymbol calls introduced by the dlloader work so     DoLoadableServer NO builds work again.

--- src/mga_storm.c
+++ src/mga_storm.c
@@ -745,7 +745,7 @@ MGANAME(AccelInit)(ScreenPtr pScreen)
 	infoPtr->SubsequentScreenToScreenColorExpandFill =
 		MGANAME(SubsequentPlanarScreenToScreenColorExpandFill);
 	infoPtr->CacheColorExpandDensity = PSZ;
-	infoPtr->CacheMonoStipple = LoaderSymbol("XAACachePlanarMonoStipple");
+	infoPtr->CacheMonoStipple = XAACachePlanarMonoStippleWeak();
 	/* It's faster to blit the stipples if you have fastbilt */
 	if(pMga->HasFBitBlt)
 	    infoPtr->ScreenToScreenColorExpandFillFlags = TRANSPARENCY_ONLY;
